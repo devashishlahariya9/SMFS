85acd4c8ee17af78b853e2613e9f258aaa161cd9
Devashish Lahariya <devashishlahariya9@gmail.com>
2022-08-11 00:01:10 +0530

    Add files via upload

---
+++ src/src/diskio.c
@@ -0,0 +1,17 @@
+#include <stdint.h>
+#include "diskio.h"
+
+void disk_readBlock(uint32_t _blockNumber, uint8_t* _buffer)
+{
+	
+}
+
+void disk_writeBlock(uint32_t _blockNumber, uint8_t* _buffer)
+{
+	
+}
+
+void disk_init(void)
+{
+    
+}
